02493c2f2a1880f7a11154411a8ccc17d50c3e54
TuanAnh080294 <tuananh080294@gmail.com>
2019-06-24 14:10:47 +0700

    validation create user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,14 +21,26 @@ module.exports.search = function(req, res) {
 };
 
 module.exports.create = function(req, res) {
-	res.render('../views/user/create');
+	res.render('user/create');
 };
 
 module.exports.postCreate = function(req, res) {
-	var user = {
-		id: shortid.generate(),
-		name: req.body.name
-	};
+	var user = req.body;
+	user.id = shortid.generate();
+	var errors = [];
+	if(!user.name) {
+		errors.push("Name is required!!!");
+	}
+	if(!user.phone) {
+		errors.push("Phone is required!!!")
+	}
+	if(errors.length) {
+		res.render("user/create", {
+			errors: errors,
+			user: user
+		})
+		return;
+	}
 	db.get('users').push(user).write();
 	res.redirect('/user');
 };
@@ -36,7 +48,7 @@ module.exports.postCreate = function(req, res) {
 module.exports.view = function(req, res) {
 	var id = req.params.id;
 	var user = db.get('users').find( {id: id}).value();
-	res.render('../views/user/view', {
+	res.render('user/view', {
 		user: user
 	});
 };
