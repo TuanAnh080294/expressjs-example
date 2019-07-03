ad6194e3e3e8a5a90a7b34bb4c8dcc7ef3f0089f
TuanAnh080294 <tuananh080294@gmail.com>
2019-07-03 22:47:11 +0700

    mongoose-user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var db = require('../db');
-var users = db.get('users').value();
-const shortid = require('shortid');
+// var express = require('express');
+// var db = require('../db');
+// var users = db.get('users').value();
+// const shortid = require('shortid');
 
-// async function getUser() {
-// 	var user = await User.find();
-// 	return user;
-// }
+var User = require("../models/user.model.js");
 
-module.exports.index = function(req, res) {
-	// var users = getUser();
+module.exports.index = async function(req, res) {
+	var users = await User.find();
 	res.render('../views/user/user', {
 		users: users
 	});
 };
 
-module.exports.search = function(req, res) {
+module.exports.search = async function(req, res) {
 	var q = req.query.q;
-	var queryMatches = users.filter(function(user) {
+	var users = await User.find();
+	var queryMatches = await users.filter(function(user) {
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
 	});
 	res.render('../views/user/user', {
@@ -31,20 +29,20 @@ module.exports.create = function(req, res) {
 	res.render('user/create');
 };
 
-module.exports.postCreate = function(req, res) {
-	var user = req.body;
-	user.id = shortid.generate();
-	user.avatar = req.file.path.split('\\').slice(1).join('/');
-	db.get('users').push(user).write();
+module.exports.postCreate = async function(req, res) {
+	avatar = req.file.path.split('\\').slice(1).join('/');
+	var user = new User({ name: req.body.name, avatar: avatar, phone: req.body.phone  });
+	await users.save();
 	// console.log(res.locals);
 	res.cookie("id","123456");
 	res.redirect('/user');
 };
 
-module.exports.view = function(req, res) {
+module.exports.view = async function(req, res) {
+	var ObjectId = require('mongoose').Types.ObjectId;
 	var id = req.params.id;
-	var user = db.get('users').find( {id: id}).value();
+	var user = await User.find({ _id: new ObjectId(id) });
 	res.render('user/view', {
-		user: user
+		user: user[0]
 	});
 };
